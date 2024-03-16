01d9f044bad8cf06b4cfd0cefcc2a72ea545252f
UlyBala <bekzhankamalov05@gmail.com>
2024-03-16 17:11:14 +0600

    ref: change folder

--- src/app/mock/supers.ts
+++ src/app/mock/supers.ts
@@ -4,4 +4,16 @@ export const supers: any[] = [
     price: '$1,000,000',
     photo: 'assets/type-auto/supers/adder/1.jpg'
   },
+
+  {
+    title: 'Bullet',
+    price: '$84,000',
+    photo: 'assets/type-auto/supers/bullet/1.jpg'
+  },
+
+  {
+    title: 'Cheetah',
+    price: '$650,000 ',
+    photo: 'assets/type-auto/supers/cheetah/1.jpg'
+  },
 ]
---
+++ src/app/shared/details-auto/details-auto.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-details-auto',
+  templateUrl: './details-auto.component.html',
+  styleUrl: './details-auto.component.scss'
+})
+export class DetailsAutoComponent {
+
+}
